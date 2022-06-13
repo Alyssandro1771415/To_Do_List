fea8fbe343f94a334da9a2a7655755c5fc4650af
Alyssandro Dyogo <100590540+Alyssandro1771415@users.noreply.github.com>
2022-06-12 22:42:12 -0300

    Update back-end.js

--- CODE/back-end.js
+++ CODE/back-end.js
@@ -10,11 +10,14 @@ btn.addEventListener("click", function(e) {
         // Criar elemento
         var txt = document.createElement('a');
 
-        // Criar o nó de texto
-        var conteudo = document.createTextNode("Um título qualquer");
+        // Criar o nó de texto abstraido do input(OBS: Aqui não está funcionando, essa linha não consegue puxar os dados do entry do HTML.)
+        var conteudo = document.querySelector("#new_item");
+
+        // Colocando o valor absorvido
+        var input = conteudo.value;
 
         // Anexar o nó de texto ao elemento h1
-        txt.appendChild(conteudo);
+        txt.appendChild(input);
 
         // Agora sim, inserir (anexar) o elemento filho (titulo) ao elemento pai (body)
         elemento_pai.appendChild(txt);
